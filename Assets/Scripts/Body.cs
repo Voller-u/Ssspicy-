ee11999be86260c1639e3a0006c97c9961ce7b72
ctcticxh <3574736597@qq.com>
2024-03-30 02:06:14 +0800

    3/20/2:05
    完成蛇的移动和第一幅地图的绘制

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -1,28 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Body : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    // Start is called before the first frame update
+    [Header("飞行状态")]
+    public bool flying;
+    [Header("身体部位贴图")]
+    public List<Sprite> sprites;
+    [Header("身体转角贴图")]
+    public List<Sprite> cornerSprites;
+    [Header("尾巴贴图")]
+    public List<Sprite> tailSprites;
+
+    public bool isTail;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Init();
+    }
+
+     void Init()
+    {
+        isTail = false;
+        var sp = GetComponent<SpriteRenderer>().sprite;
+        foreach(var sprite in tailSprites)
+        {
+            if(sp == sprite) isTail = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.W)) transform.Translate((Vector3)Vector2.up);
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    rb.velocity = new Vector2(-1, 0);
-        //}
-        //if (Input.GetKeyDown(KeyCode.S)) transform.Translate((Vector3)Vector2.down);
-        //if (Input.GetKeyDown(KeyCode.D)) transform.Translate((Vector3)Vector2.right);
-        transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, 0);
+        
+    }
+    private enum TailDir { right,down,left,up}
+    public void UpdateSprite(Vector3 left,Vector3 right)
+    {
+        
+        if(isTail == true)
+        {
+            if (left.x < transform.position.x && left.y == transform.position.y)
+                GetComponent<SpriteRenderer>().sprite = tailSprites[(int)TailDir.right];
+
+            if (left.x > transform.position.x && left.y == transform.position.y)
+                GetComponent<SpriteRenderer>().sprite = tailSprites[(int)TailDir.left];
+
+            if (left.x == transform.position.x && left.y > transform.position.y)
+                GetComponent<SpriteRenderer>().sprite = tailSprites[(int)TailDir.down];
+
+            if (left.x == transform.position.x && left.y < transform.position.y)
+                GetComponent<SpriteRenderer>().sprite = tailSprites[(int)TailDir.up];
+        }
+        else
+        {
+            Vector3 pos = transform.position;
+            Vector3 vec = (left - pos) + (right - pos);
+             
+            Debug.Log(vec);
+            if(vec.x > 0 && vec.y >0)
+            {
+                GetComponent<SpriteRenderer>().sprite = cornerSprites[2];
+            }
+            if (vec.x > 0 && vec.y < 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = cornerSprites[0];
+            }
+            if (vec.x < 0 && vec.y > 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = cornerSprites[3];
+            }
+            if (vec.x < 0 && vec.y < 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = cornerSprites[1];
+            }
+
+            if ((left.x < pos.x && right.x > pos.x) || (left.x > pos.x && right.x < pos.x))
+                GetComponent<SpriteRenderer>().sprite = sprites[0];
+            if ((left.y < pos.y && right.y > pos.y) || (left.y > pos.y && right.y < pos.y))
+                GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [Header("飞行属性")]
+    public bool flying;
+    [Header("身体部位")]
+    public List<Body> bodies = new List<Body>();
+    [Header("头部贴图")]
+    public List<Sprite> sprites;
+    //头部朝向
+    public Vector3 orient;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitSprite();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputKey();
+    }
+
+    /// <summary>
+    /// 初始化蛇的头部朝向
+    /// </summary>
+    void InitSprite()
+    {
+        int index = 0; 
+        for(int i =0;i<sprites.Count;i++)
+        {
+            if(GetComponent<SpriteRenderer>().sprite == sprites[i])
+            {
+                index = i;
+                break;
+            }
+        }
+        switch(index)
+        {
+            case 0: orient = Vector3.right; break;
+            case 1: orient = Vector3.down; break;
+            case 2: orient = Vector3.left; break;
+            case 3: orient = Vector3.up; break;
+        }
+    }
+    /// <summary>
+    /// 根据蛇的移动方向dir调整蛇头的贴图
+    /// </summary>
+    /// <param name="dir">移动方向</param>
+    void UpdateSprite(Vector3 dir)
+    {
+        //if (dir == orient) 
+        //    return;
+        if(dir == Vector3.right)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        if (dir == Vector3.down)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+        if (dir == Vector3.left)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[2];
+        }
+        if (dir == Vector3.up)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[3];
+        }
+
+        orient = dir;
+
+        //更新身体的贴图
+        List<Vector3> ts = new List<Vector3>();
+        ts.Add(transform.position);
+        foreach(var b in bodies) ts.Add(b.transform.position);
+        ts.Add(Vector3.zero);
+        for(int i=0;i<bodies.Count;i++)
+        {
+            bodies[i].UpdateSprite(ts[i], ts[i + 2]);
+        }
+    }
+
+    void InputKey()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            if ((Vector3.up + orient).x == 0 && (Vector3.up + orient).y == 0) return;
+            Move(Vector3.up);
+        }
+
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            if ((Vector3.down + orient).x == 0 && (Vector3.down + orient).y == 0) return;
+            Move(Vector3.down);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if ((Vector3.right + orient).x == 0 && (Vector3.right + orient).y == 0) return;
+            Move(Vector3.right);
+        }
+
+        else if( Input.GetKeyDown(KeyCode.A)) 
+        {
+            if ((Vector3.left + orient).x == 0 && (Vector3.left + orient).y == 0) return;
+            Move(Vector3.left);
+        }
+    }
+
+    /// <summary>
+    /// 向特定方向移动
+    /// </summary>
+    /// <param name="dir">方向向量</param>
+    void Move(Vector3 dir)
+    {
+        //头部和身体各个部位的位置
+        List<Vector3> poses = new List<Vector3>();
+        poses.Add(transform.position);
+        foreach(Body b in bodies)
+        {
+            poses.Add(b.transform.position);
+        }
+        transform.Translate(dir);
+        for(int i=0;i<bodies.Count;i++) 
+        {
+            Vector3 targetPos = poses[i];
+            bodies[i].transform.position = new Vector3(targetPos.x, targetPos.y, targetPos.z);
+        }
+        UpdateSprite(dir);
+    }
+}
